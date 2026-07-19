65fecda81e9d886b187ce6ecf83887deaa01e6a2
Lee renmos <88010408+Blackleets@users.noreply.github.com>
2026-07-19 18:57:54 +0200

    feat(storage): export persistent storage adapters

---
+++ packages/kernel/src/storage/index.ts
@@ -0,0 +1,5 @@
+export * from './atomic-json-file';
+export * from './backup-data-directory';
+export * from './json-case-repository';
+export * from './json-evidence-repository';
+export * from './storage-errors';
