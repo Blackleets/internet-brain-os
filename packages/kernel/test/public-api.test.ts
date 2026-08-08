8162e1fd5d7860667057db4afb94c4e82a1b5c59
Lee renmos <88010408+Blackleets@users.noreply.github.com>
2026-08-08 15:54:42 +0200

    test(kernel): lock proposed plan public API exports

--- packages/kernel/test/public-api.test.ts
+++ packages/kernel/test/public-api.test.ts
@@ -4,14 +4,21 @@ import {
   CaseAlreadyExistsError,
   CaseManager,
   CaseNotFoundError,
+  GoalNotFoundForPlanError,
   InvalidCaseInputError,
   InvalidCaseTransitionError,
+  ProposedPlanCapabilityDeniedError,
+  ProposedPlanManager,
+  ProposedPlanRevisionConflictError,
   StaleCaseUpdateError,
 } from '../src';
 import type {
   CaseRepository,
   CreateCaseInput,
+  CreateProposedPlanInput,
+  UniversalGoal,
   UpdateCaseInput,
+  UpdateProposedPlanInput,
 } from '../src';
 
 describe('kernel public API', () => {
@@ -25,12 +32,28 @@ describe('kernel public API', () => {
     expect(StaleCaseUpdateError).toBeTypeOf('function');
   });
 
-  test('exports compile-time contracts', () => {
+  test('exports compile-time Case contracts', () => {
     const acceptTypes = (
       _repository: CaseRepository,
       _create: CreateCaseInput,
       _update: UpdateCaseInput,
     ): void => undefined;
     expect(acceptTypes).toBeTypeOf('function');
+  });
+
+  test('exports Proposed Plan runtime boundaries', () => {
+    expect(ProposedPlanManager).toBeTypeOf('function');
+    expect(GoalNotFoundForPlanError).toBeTypeOf('function');
+    expect(ProposedPlanCapabilityDeniedError).toBeTypeOf('function');
+    expect(ProposedPlanRevisionConflictError).toBeTypeOf('function');
+  });
+
+  test('exports compile-time Goal and Proposed Plan contracts', () => {
+    const acceptPlanTypes = (
+      _goal: UniversalGoal,
+      _create: CreateProposedPlanInput,
+      _update: UpdateProposedPlanInput,
+    ): void => undefined;
+    expect(acceptPlanTypes).toBeTypeOf('function');
   });
 });
